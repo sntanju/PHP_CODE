8983f4c98a7aae0f675c83622a818d9052b49313
Sanjida Nasrin <sntanju07@gmail.com>
2024-07-10 18:29:02 +0600

    initial commit for assignment 3

---
+++ Assignment3Temlate/login.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $usersFile = 'allData/allUsers.json';
+    $users = json_decode(file_get_contents($usersFile), true);
+
+    foreach ($users as $user) {
+        if ($user['email'] === $email && password_verify($password, $user['password'])) {
+            header('Location: dashboard.php');
+            exit();
+        }
+    }
+
+    echo 'Invalid credentials';
+}
+?>
---
+++ Assignment3Temlate/register.php
@@ -0,0 +1,34 @@
+<?php
+
+function generateUID() {
+    return uniqid();
+}
+
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $uid = generateUID(); 
+
+    $usersFile = 'allData/allUsers.json';
+    $users = json_decode(file_get_contents($usersFile), true);
+
+    $users[] = [
+        'name' => $name,
+        'email' => $email,
+        'password' => $password,
+        'uid' => $uid
+    ];
+
+    file_put_contents($usersFile, json_encode($users));
+
+    session_start();
+    $_SESSION['uid'] = $uid;
+    $_SESSION['shareable_link'] = "http://localhost:8000/feedback.php?uid=" . $uid;
+
+    header('Location: dashboard.php');
+    exit();
+}
+?>
